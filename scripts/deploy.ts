7d9ea3b2485eceed55056a2d924b0a4e3c3a122d
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-05-03 20:52:14 -0300

    feat: deploy hero on rinkeby with verification

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+async function verify(contractAddress: string) {
+  return await run("verify:verify", {
+    address: contractAddress,
+    constructorArguments: [],
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function main() {
+  const Hero = await ethers.getContractFactory("Hero");
+  const hero = await Hero.deploy();
+  await hero.deployed();
+  console.log("Hero deployed to:", hero.address);
+
+  await hero.deployTransaction.wait(7);
+  await verify(hero.address);
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
