3254eaa42ff370cccf552b0b11b0f3e7a1d840a9
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-05-03 22:28:05 -0300

    feat: add openzeppelin defender relay integration

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -6,25 +6,51 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Enumer
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract Hero is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable {
+contract Hero is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
 
+    address private _defender;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
-    function initialize() public initializer {
+    function initialize(address defender) public initializer {
+        require(defender != address(0), "Defender address can't be zero");
+        _defender = defender;
         __ERC721_init("Hero", "HERO");
         __ERC721Enumerable_init();
         __Ownable_init();
     }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(
+        address to,
+        bytes32 hash,
+        bytes memory signature
+    ) 
+        nonReentrant
+        onlyOwner
+        public 
+    {
         uint256 tokenId = _tokenIdCounter.current();
+        require(
+            hash == keccak256(abi.encode(msg.sender, tokenId, address(this))),
+            "Invalid hash"
+        );
+        require(
+            ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), signature) == _defender,
+            "Invalid signature"
+        );
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+    }
+
+    function getCurrentTokenCount() public view returns (uint256) {
+        return _tokenIdCounter.current();
     }
 
     // The following functions are overrides required by Solidity.
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,32 @@
-import { ethers, run } from "hardhat";
+import { ethers, run, upgrades } from "hardhat";
 
-async function verify(contractAddress: string) {
+const HERO_NOBOTS_MINTER_RELAYER_ADDRESS = process.env
+  .HERO_NOBOTS_MINTER_RELAYER_ADDRESS as string;
+
+async function verify(
+  contractAddress: string,
+  constructorArguments: string[] = []
+) {
   return await run("verify:verify", {
     address: contractAddress,
-    constructorArguments: [],
+    constructorArguments,
   });
 }
 
 async function main() {
   const Hero = await ethers.getContractFactory("Hero");
-  const hero = await Hero.deploy();
+  const hero = await upgrades.deployProxy(Hero, [
+    HERO_NOBOTS_MINTER_RELAYER_ADDRESS,
+  ]);
+
   await hero.deployed();
   console.log("Hero deployed to:", hero.address);
 
-  await hero.deployTransaction.wait(7);
-  await verify(hero.address);
+  const txReceipt = await hero.deployTransaction.wait(7);
+  const heroProxiedContractAddress = ethers.utils.hexStripZeros(
+    txReceipt.logs[0].topics[1]
+  );
+  await verify(heroProxiedContractAddress);
 }
 
 main().catch((error) => {
